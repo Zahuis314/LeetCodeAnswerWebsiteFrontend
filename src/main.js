617abd8a26c9327af6f7379060d181c8d5e764d0
Roberto Zahuis <zahuis314@gmail.com>
2022-01-16 20:07:27 -0500

    Add Axios and BootstrapVue

--- src/main.js
+++ src/main.js
@@ -2,9 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import axios from "axios";
+import { BootstrapVue } from "bootstrap-vue";
 Vue.config.productionTip = false;
 
+const axiosBase = axios.create({
+  baseURL: process.env.VUE_APP_BACKEND_API_URL,
+});
+Vue.prototype.$axios = axiosBase;
+Vue.use(BootstrapVue);
 new Vue({
   router,
   store,
