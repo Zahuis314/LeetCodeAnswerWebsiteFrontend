62689ed58fba5d4ef552ff08365078357c37b8f5
Roberto Zahuis <zahuis314@gmail.com>
2022-01-21 22:40:47 -0500

    Change problems page

--- src/views/Problems.vue
+++ src/views/Problems.vue
@@ -14,22 +14,41 @@
         :sort-by.sync="sortBy"
         :sort-desc.sync="sortDesc"
         responsive="sm"
-      />
+      >
+        <template #cell(title)="data">
+          {{ data.value }}
+          <b-icon-file-earmark-lock
+            class=""
+            variant="warning"
+            font-scale="1.35"
+            v-if="data.item.is_paid_only"
+          >
+          </b-icon-file-earmark-lock>
+        </template>
+        <template #cell(acceptance)="data">
+          {{ data.item.ac_rate.toFixed(1) }}%
+        </template>
+      </b-table>
     </b-overlay>
   </div>
 </template>
 
 <script>
+import { BIconFileEarmarkLock } from "bootstrap-vue";
+
 export default {
   name: "Problems",
+  components: { BIconFileEarmarkLock },
   data() {
     return {
-      problems: [{}],
-      sortBy: "age",
+      problems: [],
+      sortBy: "question_frontend_id",
       sortDesc: false,
       fields: [
+        { key: "question_frontend_id", label: "Id", sortable: true },
         { key: "title", sortable: true },
         { key: "difficulty", sortable: true },
+        { key: "acceptance", sortable: true },
       ],
       loading: true,
     };
