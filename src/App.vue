cfa276f8f8056c06591accd5afd42b5ca44260bd
Roberto Zahuis <zahuis314@gmail.com>
2022-01-27 19:47:23 -0500

    Add brand with css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <b-navbar variant="primary">
+      <b-navbar-brand id="animation-title" to="/">
+        <div id="animation-leet">Leet</div>
+        <div id="animation-tile">
+          <div id="animation-think"></div>
+        </div>
+      </b-navbar-brand>
       <b-container>
         <b-navbar-nav>
           <router-link class="router-link" tag="li" :to="{ name: 'Problems' }">
@@ -43,5 +49,42 @@ export default {
 body {
   --bs-body-bg: ghostwhite;
   --bs-body-color: darkslategray;
+}
+#animation-title{
+  font-family: monospace;
+  display: flex;
+  width: 140px;
+  margin-left: 20px;
+}
+#animation-think::after{
+  white-space: nowrap;
+  overflow: hidden;
+  display: flex;
+  content: "";
+  animation: typewriting 9s steps(5,start) infinite, blinking 1s infinite;
+  border-right: 2px solid;
+}
+#animation-tile{
+  display: inline;
+}
+@keyframes typewriting {
+  0% { width: 0%; content: "Think"; animation-timing-function: steps( 5,start ) ; };
+  6.25% { width: 100%; };
+  21.25% { width: 100%; };
+  24.99% { width: 0%; content: "Think"; };
+  25.00% { width: 0%; content: "Design"; animation-timing-function: steps( 6,start ) ; };
+  31.25% { width: 100%; };
+  46.25% { width: 100%; };
+  49.99% { width: 0%; content: "Design";}  ;
+  50.00% { width: 0%; content: "Code"; animation-timing-function: steps( 4,start ) ; };
+  65% { width: 100%; };
+  92.50% { width: 100%; };
+  100% { width: 0%; content: "Code"; };
+}
+@keyframes blinking {
+  0% { border-color: black; }
+  45% { border-color: black; }
+  45.001% { border-color: transparent }
+  100% { border-color: transparent }
 }
 </style>
